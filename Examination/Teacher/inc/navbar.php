afa342e832498150954a5628330dbcc729ac565e
Nima640 <dmnddissanayaka@std.appsc.sab.ac.lk>
2024-12-10 10:25:03 +0530

    added navbar for teacher

---
+++ Examination/Teacher/inc/navbar.php
@@ -0,0 +1,50 @@
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="../logo.png" width="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navLinks">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Dashboard</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="addmarks.php">Add Table</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="marks.php">Add Marks</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="updatemarks.php">Update Grade</a>
+        </li>
+        <li class="nav-item">
+          
+          <a class="nav-link" href="teacher_requests.php">Recorrection Requests
+            <?php if ($newRequestsCount > 0) : ?>
+              <span class="badge bg-danger"><?= $newRequestsCount ?></span>
+            <?php endif; ?>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="completedreq.php">Completed Requests</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pass.php">Change Password</a>
+        </li>
+
+      </ul>
+      <ul class="navbar-nav me-right mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="../logout.php">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
